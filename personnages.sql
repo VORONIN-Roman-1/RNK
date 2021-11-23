aee1c330295d56264954b7285dfd43d597ddd862
Karo <karapet.avakian@gmail.com>
2021-11-23 23:24:20 +0100

    SQL- personnages complétés_45!!!

--- personnages.sql
+++ personnages.sql
@@ -3,4 +3,49 @@ INSERT INTO `personnages` VALUES
 	(2,'War Machine','photoWarMachine','lieutenant-colonel James « Rhodey » Rhodes'),
 	(3,'Pepper Potts','photoPepperPotts','Pepper Potts'),
     (4, 'Hulk','photoHulk', 'Bruce Banner'),
-    (5, 'Nick Fury','photoNickFury','Nick Fury');
+    (5, 'Nick Fury','photoNickFury','Nick Fury'),
+    (7, 'Black Widow','photoBlackWidow',''),---
+    (8, 'Thor','photoThor','Thor'),
+    (9, 'Odin','photoOdin','Odin'),
+    (10, 'Jane Foster','photoJaneFoster','Jane Foster'),
+    (11, 'Heimdall','photoHeimdall','Heimdall'),
+    (12, 'Loki','photoLoki','Loki'),
+    (13, 'Hawkeye','photoHawkeye','Hawkeye'),
+    (14, 'Captain America','photoCaptainAmerica',''),
+    (15, 'Le Soldat de l hiver','photoLeSoldatDeLhiver',''), ---
+    (16, 'Thanos','photoThanos','Thanos'),
+    (17, 'Le Collectionneur','photoLeCollectionneur','Le Collectionneur'),
+    (18, 'Le Faucon','photoLeFaucon','Le Faucon'),-----
+    (19, 'Wanda Maximoff','photoWandaMaximoff','Wanda Maximoff'),
+    (20, 'Star-Lord','photoStarLord','Star-Lord'),------
+    (21, 'Gamora','photoGamora','Gamora'),
+    (22, 'Rocket','photoRocket','Rocket'),
+    (23, 'Groot','photoGroot','Groot'),
+    (24, 'Drax','photoDrax','Drax'),
+    (25, 'Yondu','photoYondu','Yondu'),
+    (26, 'Nebula','photoNebula','Nebula'),
+    (27, 'Ultron','photoUltron','Ultron'),
+    (28, 'Pietro Maximoff','photoPietroMaximoff','Pietro Maximoff'),
+    (29, 'Ant-Man','photoAntMan','Ant-Man'),-----
+    (30, 'La Guêpe','photoLaGuêpe','La Guêpe'),----
+    (31, 'Spider-Man','photoSpiderMan','Spider-Man'),----
+    (32, 'Black Panther','photoBlackPanther','Black Panther'),-------
+    (33, 'Vision','photoVision','Vision'),
+    (34, 'Docteur Strange','photoDocteurStrange','Docteur Strange'),----
+    (35, 'Mordo','photoMordo','Mordo'),
+    (36, 'Wang','photoWang','Wang'),
+    (37, 'Mantis','photoMantis','Mantis'),
+    (38, 'Valkyrie','photoValkyrie','Valkyrie'),
+    (39, 'Nakia','photoNakia','Nakia'),
+    (40, 'Okoye','photoOkoye','Okoye'),
+    (41, 'Shuri','photoShuri','Shuri'),
+    (42, 'W Kabi','photoWKabi','W Kabi'),
+    (43, 'Captain Marvel','photoCaptainMarvel','Captain Marvel'),
+    (44, 'Shang-Chi','photoShangChi','Shang-Chi'),
+    (45, '','photo',''),
+    (46, '','photo',''),
+    (47, '','photo',''),
+    (48, '','photo','') ;
+    
+    
+    
