207bd934e606e19b210653d9e6ebb778128b6af6
Karo <karapet.avakian@gmail.com>
2021-11-23 16:51:36 +0100

    Fichier SQL de 5 personnages

---
+++ personnages.sql
@@ -0,0 +1,6 @@
+INSERT INTO `personnages` VALUES 
+	(1,'Iron Man','photoIronMan','Tony Stark'),
+	(2,'War Machine','photoWarMachine','lieutenant-colonel James « Rhodey » Rhodes'),
+	(3,'Pepper Potts','photoPepperPotts','Pepper Potts'),
+    (4, 'Hulk','photoHulk', 'Bruce Banner'),
+    (5, 'Nick Fury','photoNickFury','Nick Fury');
