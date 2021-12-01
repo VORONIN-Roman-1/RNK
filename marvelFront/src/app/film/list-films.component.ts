b927a4fa06b77394400d44de6c96007a9ca2f223
VORONIN Roman <voronin.roman.i@gmail.com>
2021-12-01 16:30:27 +0100

    version qui marche

--- marvelFront/src/app/film/list-films.component.ts
+++ marvelFront/src/app/film/list-films.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 //  import * as M from "../../assets/materialize/js/materialize.min.js";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Film } from "./film";
 import { FilmService } from "./film.service";
+import * as M from 'materialize-css';
 
 
-@Component ({
+@Component({
     selector: 'list-films',
     templateUrl: './list-films.component.html'
     //styleUrls: ['./list-films.component.css']
 })
-export class ListFilmsComponent implements OnInit {
-
+export class ListFilmsComponent implements OnInit, AfterViewInit {
+    condition = true;
     films!: Film[];
+    options = { fullWidth: false };
 
-    constructor( 
+
+    constructor(
         private route: ActivatedRoute,
-        private router : Router,private filmService: FilmService) { }
-  
-   
-    ngOnInit() : void { 
-      this.getFilms();
+        private router: Router, private filmService: FilmService) { }
+
+    ngOnInit(): void {
+        this.getFilms();
+    }
+    ngAfterViewInit() {
     }
 
-    getFilms(): void  {
-        this.filmService.getFilms().subscribe( films  => this.films= films);
+    getCarousel() {
+        let elems = document.querySelectorAll('.carousel');
+        let instances = M.Carousel.init(elems, this.options);
+         setTimeout(() => {
+            this.condition = false;
+        }, 0);
     }
 
-    selectFilm (film : Film){
-        alert("vous avez cliqué sur le film : "+ film.title)
-        let link = [ '/film', film.id];
+    getFilms(): void {
+        this.filmService.getFilms().subscribe(films => {
+            this.films = films;
+        });
+    }
+
+    selectFilm(film: Film) {
+        alert("vous avez cliqué sur le film : " + film.title)
+        let link = ['/film', film.id];
         this.router.navigate(link);
-      }
+    }
 }
